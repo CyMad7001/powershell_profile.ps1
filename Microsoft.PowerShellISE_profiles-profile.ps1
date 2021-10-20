a77a4d7e8b590e4048de14bbae5797c70cc76c4a
CyMad7001 <90966823+CyMad7001@users.noreply.github.com>
2021-10-20 01:32:34 +0100

    Create Microsoft.PowerShellISE_profiles-profile.ps1

---
+++ Microsoft.PowerShellISE_profiles-profile.ps1
@@ -0,0 +1,22 @@
+## Load required profiles
+
+Load-Profile "modules" -Quiet
+Load-Profile "paths" -Quiet
+
+## Load usefull profiles
+
+Load-Profile "b64"
+Load-Profile "dotnet"
+Load-Profile "docker"
+Load-Profile "git"
+Load-Profile "json"
+Load-Profile "local"
+Load-Profile "oh-my-posh"
+Load-Profile "psreadline"
+Load-Profile "vim"
+
+## SECURITY - SENSITIVE DATA
+
+Load-Profile "secret" -Quiet
+
+## SENSITIVE DATA
